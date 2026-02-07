09a4db8f9e47a45dc798e699f852310b06aafcb9
mfstapert <marijn.stapert@xip.nl>
2026-02-08 00:07:26 +0100

    test setup

--- apps/angular-client-app-e2e/src/example.spec.ts
+++ apps/angular-client-app-e2e/src/example.spec.ts
@@ -6,4 +6,13 @@ test('has title', async ({ page }) => {
 
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
+
+  // Doesn't work because reporter cleans coverage dir at start of run
+  // const fixtureContent = readFileSync(join(__dirname, '../fixtures/lcov.info'), 'utf-8');
+  // const reporterContent = readFileSync(
+  //   join(__dirname, '../../../coverage/playwright-coverage-reporter/lcov.info'),
+  //   'utf-8',
+  // );
+  //
+  // expect(fixtureContent).toBe(reporterContent);
 });
