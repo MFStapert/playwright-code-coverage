53cc782b3d2238bef767987f3bb201e9e7b093ee
mfstapert <marijn.stapert@xip.nl>
2026-02-08 02:50:34 +0100

    angular server app boilerplate

---
+++ apps/angular-server-app/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'server-root',
+  templateUrl: './app.html',
+})
+export class App {}
