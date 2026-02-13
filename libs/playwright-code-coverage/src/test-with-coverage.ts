8ff3b5cdea74c41e72b5ab1e4ebec33341a9d498
mfstapert <marijn.stapert@xip.nl>
2026-02-13 23:15:06 +0100

    add chromium check

--- libs/playwright-code-coverage/src/test-with-coverage.ts
+++ libs/playwright-code-coverage/src/test-with-coverage.ts
@@ -2,12 +2,16 @@ import { test } from '@playwright/test';
 import { coverageFileKey, marshallCoverage } from './utils/attachment.utils';
 
 export const testWithCoverage = test.extend({
-  page: async ({ page }, use, testInfo) => {
-    await page.coverage.startJSCoverage();
+  page: async ({ context, page }, use, testInfo) => {
+    if (context.browser()?.browserType()?.name() === 'chromium') {
+      await page.coverage.startJSCoverage();
 
-    await use(page);
+      await use(page);
 
-    const coverage = await page.coverage.stopJSCoverage();
-    await testInfo.attach(coverageFileKey, marshallCoverage(coverage));
+      const coverage = await page.coverage.stopJSCoverage();
+      await testInfo.attach(coverageFileKey, marshallCoverage(coverage));
+    } else {
+      console.info('Skipped coverage after test run for non chromium browser...');
+    }
   },
 });
