c485d2ea5ea92134cf9817e3a5412121511052db
mfstapert <marijn.stapert@xip.nl>
2026-02-13 22:31:52 +0100

    less noisy test script

--- test.js
+++ test.js
@@ -9,8 +9,11 @@ const tests = [
 ];
 
 for (const test of tests) {
-  execSync(`npx nx run ${test.command}`, { stdio: 'inherit' });
+  console.info('Running testcase for: ' + test.command);
+  execSync(`npx nx run ${test.command}`, { stdio: 'ignore' });
   const file1 = readFileSync('coverage/playwright-code-coverage/lcov.info', 'utf8');
   const file2 = readFileSync(`fixtures/${test.fixture}`, 'utf8');
   strictEqual(file1, file2, 'Files must be identical');
+  console.info('Test passed');
 }
+console.info('All tests passed');
